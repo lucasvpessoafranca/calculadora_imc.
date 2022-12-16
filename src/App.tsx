610015e28c9bd6be59a00ff7f471dff317f3c241
Lucas França <lucasvpessoafranca@gmail.com>
2022-12-16 20:11:45 -0300

    finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import styles from './App.module.css'
 import logo from './assets/powered.png'
 import {GridItem} from './components/GridItem'
 import { levels, calculateImc, Level } from './helpers/imc'
+import lefArrowImage from './assets/leftarrow.png'
 export function App() {
   const [heightField, setHeightField] = useState<number>(0)
   const [weightField, setWeightField] = useState<number>(0)
@@ -16,6 +17,13 @@ export function App() {
       alert("Preencha algo")
     }
   }
+
+  const handleBackButton = ()=>{
+    setToShow(null)
+    setWeightField(0)
+    setHeightField(0)
+
+  } 
   return (
     <div className={styles.main}>
       <header>
@@ -27,10 +35,10 @@ export function App() {
       <div className={styles.leftSide}>
         <h1>Calcule o seu IMC.</h1>
         <p>IMC é a sigla para Índice de Massa Corpórea, parâmetro adotado pela Organização Mundial de Saúde para calcular o peso ideal de cada pessoa.</p>
-        <input type="number" placeholder='Digite a sua Altura. EX: 1.5 (em metros)' value={heightField > 0 ? heightField : ''} onChange={e => setHeightField(parseFloat(e.target.value))} />
-        <input type="number" placeholder='Digite o seu Peso EX: 60.6 (em kg)' value={weightField > 0 ? weightField : ''} onChange={e => setWeightField(parseFloat(e.target.value))} />
+        <input disabled={toShow ? true : false} type="number" placeholder='Digite a sua Altura. EX: 1.5 (em metros)' value={heightField > 0 ? heightField : ''} onChange={e => setHeightField(parseFloat(e.target.value))} />
+        <input disabled={toShow ? true : false} type="number" placeholder='Digite o seu Peso EX: 60.6 (em kg)' value={weightField > 0 ? weightField : ''} onChange={e => setWeightField(parseFloat(e.target.value))} />
 
-        <button onClick={handleCalculateButton}>Calcular</button>
+        <button  onClick={handleCalculateButton} disabled={toShow ? true : false}>Calcular</button>
       </div>
       <div className={styles.rightSide}>
 
@@ -41,7 +49,9 @@ export function App() {
       </div>}
 
       {toShow && <div className={styles.rightBig}>
-            <div className={styles.rightArrow}></div>
+            <div className={styles.rightArrow} onClick={handleBackButton}>
+              <img src={lefArrowImage} alt="" width={25} />
+            </div>
             <GridItem item={toShow}/>
           </div>}
 
